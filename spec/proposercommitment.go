2d411e5253315668d6e40ca21641e9754088edc5
Bharath Vedartham <vedabharath12345@gmail.com>
2023-08-25 20:36:39 +0530

    add an array specifying the valid proposer commitment list

--- spec/proposercommitment.go
+++ spec/proposercommitment.go
@@ -6,3 +6,8 @@ const (
 	FULL_BLOCK    ProposerCommitment = "full_block"
 	TOB_ROB_SPLIT ProposerCommitment = "tob_rob_split"
 )
+
+var VALID_PROPOSER_COMMITMENTS = [2]ProposerCommitment{
+	FULL_BLOCK,
+	TOB_ROB_SPLIT,
+}
